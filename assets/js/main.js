a0295b6acdd96e3b99481fb9fd85df00ec89282d
Estêvão Abreu <estevao.abreu40@gmail.com>
2025-08-17 19:03:33 +0100

    Fixed stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,50 @@
-jQuery(window).on('load', function() {
-	"use strict";
-    
-    
-    // HIDE PRELOADER
-    $(".preloader").addClass("hide-preloader");   
-    
-    // SHOW/ANIMATE ANIMATION CONTAINER
-    setTimeout(function(){
+jQuery(window).on('load', function () {
+    "use strict";
 
-        $("#intro .animation-container").each(function() {
+
+    // HIDE PRELOADER
+    $(".preloader").addClass("hide-preloader");
+
+    // SHOW/ANIMATE ANIMATION CONTAINER
+    setTimeout(function () {
+
+        $("#intro .animation-container").each(function () {
 
             var e = $(this);
 
-            setTimeout(function(){
+            setTimeout(function () {
 
                 e.addClass("run-animation");
 
-            }, e.data("animation-delay") );
+            }, e.data("animation-delay"));
 
         });
 
-    }, 700 );
+    }, 700);
 
-    
+
 });
 
 
-jQuery(document).ready(function($) {
-	"use strict";
-    
-    
+jQuery(document).ready(function ($) {
+    "use strict";
+
+
     // SMOOTH SCROLL FOR SAME PAGE LINKS
-    $(document).on('click', 'a.smooth-scroll', function(event) {
-        
+    $(document).on('click', 'a.smooth-scroll', function (event) {
+
         event.preventDefault();
 
         $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top - 80
+            scrollTop: $($.attr(this, 'href')).offset().top - 80
         }, 500);
-        
+
     });
-    
-    
+
+
     // SCROLL REVEAL SETUP
     window.sr = ScrollReveal();
-    sr.reveal(".scroll-animated", { 
+    sr.reveal(".scroll-animated", {
         duration: 600,
         delay: 0,
         origin: "left",
@@ -55,5 +55,41 @@ jQuery(document).ready(function($) {
         scale: 1,
     });
 
-    
+
+    $(document).ready(function() {
+    $('#contact-form').submit(function(e) {
+        e.preventDefault();
+
+        var form = $(this);
+        var postdata = form.serialize(); 
+
+        $.ajax({
+            type: "POST",
+            url: "https://formspree.io/f/mldlknzv",
+            data: postdata,
+            dataType: "json",
+            headers: {
+                'Accept': 'application/json'
+            },
+            
+            success: function(response) {
+                if (response.ok) {
+                    form.addClass("success");
+                    form.find('input, textarea').val('');
+
+                    setTimeout(function() {
+                        form.removeClass("success");
+                    }, 4000);
+                }
+            },
+
+            error: function(xhr, status, error) {
+                console.error("Form submission failed:", status, error);
+                alert("Ocorreu um erro ao enviar o formulário. Por favor, tente novamente.");
+            }
+        });
+    });
+});
+
+
 });
